fb63bb56483d641f007dc42fa94a8de206395099
igitgud33 <altairnoquia16@gmail.com>
2025-10-07 17:16:52 +0800

    c.

--- Main/Combat.java
+++ Main/Combat.java
@@ -2,8 +2,12 @@ package Main;
 
 public class Combat {
     //Battle mechanics
+    
     //Damage calculations
+
     //Battle UI
+
     //Victory/defeat conditions
+    
     //Skill cooldown and mana management
 }
